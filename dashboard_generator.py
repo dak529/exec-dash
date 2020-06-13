d4ace26c6d30f9b56351f6853fc9637aa8c276dd
dak529 <dak529@stern.nyu.edu>
2020-06-13 14:47:33 -0400

    Added Horizontal bar graph visualization

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -64,3 +64,25 @@ for x in top_products:
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
+
+#Data Visualization
+
+# Graph definitions w/ matplotlib
+products = list(product_totals.index)
+sales = list(round(product_totals['sales price'],2))
+y_pos = np.arange(len(products))
+x_lab = [f"${x:,.2f}" for x in range(0, (round(math.ceil(sales[0] / 1000)) * 1000) + 1000, 1000)]
+
+
+#Visualization, horizontal bar graph build
+fig, ax = plt.subplots()
+
+ax.barh(y_pos, sales, xerr=0, align='center')
+ax.set_yticks(y_pos)
+ax.set_yticklabels(products)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('In-the-Month Sales (USD)', fontweight = 'bold')
+ax.set_ylabel('Products', fontweight = 'bold')
+ax.set_title('Top Selling Products (March 2019)', fontweight = 'bold')
+
+plt.show()
